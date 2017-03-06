2c00eb771278bb6a8c34617487b6982929bf8891
Luiz Silveira <luiz.silveira@>
2017-03-06 19:22:04 -0300

    Mutua Theming -- first themable version. Some modules and the default css/scss will come on the next commit

--- tools/tasks/project/build.mutua.ts
+++ tools/tasks/project/build.mutua.ts
@@ -20,6 +20,9 @@ import BuildConfig from '../../config';
 
 import { Config } from '../../../src/client/app/shared/core/utils/config';
 import { DataManipulation } from '../../config/mutua.available.modules.and.components.config';
+import { appTheme }         from '../../config/mutua.instance-project.config';
+
+import * as merge from 'merge-stream';
 
 export = () => {
 
@@ -36,10 +39,34 @@ export = () => {
     console.log(errorMsg);
     throw new Error(errorMsg);
   } else /* it is for either web or desktop */ {
-    src.push(...DataManipulation.getActivatedModulesAndComponentsHTMLFiles(Config.IS_WEB(), Config.IS_DESKTOP()),
-             ...DataManipulation.getHTMLTheme);
+    src.push(...DataManipulation.getActivatedModulesAndComponentsHTMLFiles(Config.IS_WEB(), Config.IS_DESKTOP()));
   }
 
-  return gulp.src(src, { base: './' } )
-    .pipe(gulp.dest(join(BuildConfig.APP_DEST)));
+  // copies modules and components declared files as well as theme files
+  // please note this is not an elegant solution for themes, but it will do for now: we are copying themes/components/**/*.html and *.scss files back to the source tree, where
+  // they used to belong to on the angular-seed-* projects (and the same for modules). For this reason, we had to add to .gitignore all *.html and *.scss files under
+  // src/client/app/components and src/client/app/shared/modules. This way, we didn't have to mess with the original 'build.html_css.ts', that would elegantly solve the metter.
+  return merge(
+
+    // copy the files modules and components declare they want to copy
+    gulp.src(src, { base: './' } )
+      .pipe(gulp.dest(join(BuildConfig.APP_DEST))),
+
+    // copy the theme files -- components *.html and *.scss
+    gulp.src([`themes/${appTheme}/components/**/*`], { base: `themes/${appTheme}`})
+      .pipe(gulp.dest(join('src/client/app'))),
+
+    // copy the theme files -- modules *.html and *.scss
+    gulp.src([`themes/${appTheme}/modules/**/*`], { base: `themes/${appTheme}`})
+      .pipe(gulp.dest(join('src/client/app/shared')))
+
+    // copy the theme files -- assets
+    // gulp.src(...
+  );
+
+/*
+  return gulp.src([`themes/${appTheme}/components/!**!/!*`], { base: `themes/${appTheme}`})
+    .pipe(gulp.dest(join(BuildConfig.APP_DEST + '/app')));
+*/
+
 };

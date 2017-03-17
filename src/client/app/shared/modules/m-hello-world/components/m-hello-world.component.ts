d4b3a62618361ff73339bfe89ac7732556a60538
Luiz Silveira <zertyz@gmail.com>
2017-03-17 00:28:42 -0300

    Tiny docs

--- src/client/app/shared/modules/m-hello-world/components/m-hello-world.component.ts
+++ src/client/app/shared/modules/m-hello-world/components/m-hello-world.component.ts
@@ -12,12 +12,17 @@ import { Input } from '@angular/core';
   templateUrl: 'm-hello-world.component.html',
   styleUrls: ['m-hello-world.component.css']
 })
+/** This class demonstrate the basics of creating a reusable component, who receives all it's from the caller.
+ *  There are two ways of passing data to this component, when used on templates elsewhere:
+ *  1) Via properties, like in: <m-hello-world hello="olá" world="mundo!"></m-hello-world>
+ *  2) Via an object:           <m-hello-world [data]="{hello: 'helly', world: 'wolrdo'}"></m-hello-world> */
 export class MHelloWorldComponent {
 
   @Input() hello: string;
   @Input() world: string;
   @Input() data: HelloData;
 
+  // called by Angular after all @Input() properties get their values
   ngOnInit() {
      if (this.data != null) {
       this.hello = this.data.hello;

937b9647c46a956b87177e472ed1d9c20a686de4
Luiz Silveira <zertyz@gmail.com>
2017-03-28 02:37:06 -0300

    Mutua Theming: Coding new component: m-forms, reasonably complete. We still need to:  2) Code the rest of components  3) Add the router outlet  4) Mobile version

--- src/client/app/shared/modules/m-hello-world/components/index.ts
+++ src/client/app/shared/modules/m-hello-world/components/index.ts
@@ -3,3 +3,4 @@ export * from './m-welcome.component';
 export * from './m-portfolio.component';
 export * from './m-markup.component';
 export * from './m-about.component';
+export * from './m-form.component';

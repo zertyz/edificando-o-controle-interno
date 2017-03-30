48ce20d367083c797f7fc5238c1c8cf76948e90f
Luiz Silveira <luiz.silveira@>
2017-03-30 16:17:45 -0300

    git bug commit -- changed files who are, in fact, identical (as it turned out, they were not. This commit fixes it).

--- src/client/app/components/app.routes.ts
+++ src/client/app/components/app.routes.ts
@@ -2,13 +2,9 @@
 import { HomeRoutes } from './home/home.routes';
 import { AboutRoutes } from './about/about.routes';
 
-import { GvHomeRoutes } from './pages/gv-home/gv-home.routes';
-
 export const routes: Array<any> = [
   ...HomeRoutes,
-  ...AboutRoutes,
-
-  ...GvHomeRoutes
+  ...AboutRoutes
 ];
 
 // note: mutuatech patches: this file won't be used and is kept for reference only.
